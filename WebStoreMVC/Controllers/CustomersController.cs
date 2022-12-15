6af2afaf2f4007d647f01388a0d413586a5bbf2f
Eric Couch <ericcouch@yahoo.com>
2022-12-15 10:33:09 -0600

    added edit page

--- WebStoreMVC/Controllers/CustomersController.cs
+++ WebStoreMVC/Controllers/CustomersController.cs
@@ -54,8 +54,58 @@ namespace WebStoreMVC.Controllers
             return View(customer);
         }
 
-        
-        
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer is null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Address,Phone,Email")] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return (_context.Customers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
         public async Task<IActionResult> Delete(int? id)
         {
             if (id is null || _context.Customers is null)
